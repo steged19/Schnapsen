0d7675579e793ccd0964dd821474bd0652678b16
pieth252 <67559476+pieth252@users.noreply.github.com>
2022-04-22 09:29:28 +0200

    Push

--- app/src/main/java/at/kaindorf/schnapsen/MainActivity.java
+++ app/src/main/java/at/kaindorf/schnapsen/MainActivity.java
@@ -3,6 +3,9 @@ package at.kaindorf.schnapsen;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
@@ -11,8 +14,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
         ///////////////////////////////////////////////
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+        /////////////Animation bei Start//////////////
+        ObjectAnimator yAnimation = ObjectAnimator.ofFloat(imageView,"y",230f);
+        ObjectAnimator xAnimation = ObjectAnimator.ofFloat(imageView,"x",100f);
+        yAnimation.setDuration(1500);
+        xAnimation.setDuration(1500);
+        Set<Animator> hashSet = new HashSet<>();
+        hashSet.add(yAnimation);
+        hashSet.add(xAnimation);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(hashSet);
+        animatorSet.start();
+        /////////////////////////////////////////////
 
     }
 }
